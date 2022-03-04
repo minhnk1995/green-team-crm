54924c649f0d8accb764cc3745aaaa3ff0bb1e6d
devNhaan <nhandeptrai137@gmail.com>
2022-03-04 22:12:21 +0700

    add connection mysql

---
+++ src/main/java/cybersoft/javabackend/crm/dbconnection/MySqlConnection.java
@@ -0,0 +1,29 @@
+package cybersoft.javabackend.crm.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnection {
+	private static Connection connection = null;
+	
+	private static final String URL = "jdbc:mysql//localhost:3306/crm";
+	private static final String USERNAME = "root";
+	private static final String PASWORD = "admin";
+	
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			if(connection == null || connection.isClosed())
+				connection = DriverManager.getConnection(URL, USERNAME, PASWORD);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver could not found.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Database connection could not establish.");
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
