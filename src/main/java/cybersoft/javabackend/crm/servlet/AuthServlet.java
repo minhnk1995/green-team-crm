b806f74598c6b33cafa3f7054249aa9917669ccc
devNhaan <nhandeptrai137@gmail.com>
2022-03-05 22:55:27 +0700

    add model and views login

---
+++ src/main/java/cybersoft/javabackend/crm/servlet/AuthServlet.java
@@ -0,0 +1,23 @@
+package cybersoft.javabackend.crm.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cybersoft.javabackend.crm.util.UrlConst;
+
+@WebServlet(name="authServlet",urlPatterns = {
+		UrlConst.AUTH_LOGIN
+		})
+public class AuthServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("WEB-INF/views/auth/login.jsp").forward(req, resp);
+	}
+}
--- src/main/java/cybersoft/javabackend/crm/util/UrlConst.java
+++ src/main/java/cybersoft/javabackend/crm/util/UrlConst.java
@@ -2,4 +2,6 @@ package cybersoft.javabackend.crm.util;
 
 public class UrlConst {
 	public static final String HOME = "/home";
+	
+	public static final String AUTH_LOGIN = "/log-in";
 }
