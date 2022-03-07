dabb92bd32b2586a06c331d1a959f15a2777aa5d
devNhaan <nhandeptrai137@gmail.com>
2022-03-07 15:01:10 +0700

    add function

---
+++ src/main/java/cybersoft/javabackend/crm/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package cybersoft.javabackend.crm.repository;
+
+import java.util.List;
+
+import cybersoft.javabackend.crm.model.User;
+
+public class UserRepository {
+	public List<User> findAll(){
+		
+		return null;
+	}
+	public void deleteById(int id) {
+		
+	
+	}
+	public void addUser() {
+		
+	
+	}
+}
